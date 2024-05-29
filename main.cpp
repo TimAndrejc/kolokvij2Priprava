423e399970ecfa24a4fd1d2643ab09989f0d58e3
Tim <tim.andrejc02@gmail.com>
2024-05-29 20:11:50 +0200

    desc and asc

--- main.cpp
+++ main.cpp
@@ -8,16 +8,25 @@ bool sortByPrice(const Fuel &f1, const Fuel &f2) {
     return f1.getPrice() > f2.getPrice();
 }
 
+bool ascendingPricesPerLiter(const Fuel &refuel1, const Fuel &refuel2) {
+    return refuel1.getPrice() < refuel2.getPrice();
+}
+
+bool descendingPricesPerLiter(const Fuel &refuel1, const Fuel &refuel2) {
+    return refuel1.getPrice() > refuel2.getPrice();
+}
+
 
 int main() {
-    //vector<Fuel> fuel = Fuel::LoadFromFile("refuel.csv");
-    /*fuel.emplace_back(Date(1, 5, 2024), 2300, 42.20, "Benzene", 23.22);
+    vector<Fuel> fuel; //= Fuel::LoadFromFile("refuel.csv");
+    fuel.emplace_back(Date(1, 5, 2024), 2300, 42.20, "Benzene", 23.22);
     fuel.emplace_back(Date(2, 6, 2025), 2300, 42.20, "Benzene", 23.23);
-    cout << fuel[0].toString();
-    Fuel::SortRefuels(fuel, sortByPrice);
-    cout << fuel[0].toString();
-    Fuel::saveToFile(fuel, "neki.csv");*/
-    cout << Fuel::avgPricePerLiter(3, 1.23, 1.23, 1.23);
+    Fuel::SortRefuels(fuel, descendingPricesPerLiter);
+    for (const Fuel &f: fuel) {
+        cout << f.toString();
+    }
+    Fuel::saveToFile(fuel, "neki.csv");
+    //cout << Fuel::avgPricePerLiter(3, 1.23, 1.23, 1.23);
 
 
 }
